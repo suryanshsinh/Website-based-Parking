45247e8e93eb51cbdeb19622629cb2ea4e400b18
Suryanshsinh Sisodiya <suryanshsisodiya3106@gmail.com>
2024-03-02 12:46:49 +0530

    fullscreen

--- scripts.js
+++ scripts.js
@@ -10,3 +10,22 @@ async function paintSlots() {
 }
 
 setInterval(paintSlots, 1000);
+
+const btn = document.getElementById('fullscreen-btn');
+
+btn.addEventListener('click', () => {
+    if (document.fullscreenElement) {
+        exitFullscreen();
+    } else {
+        enterFullscreen(document.documentElement);
+    }
+});
+
+function enterFullscreen(element) {
+    element.requestFullscreen?.();
+}
+
+function exitFullscreen() {
+    document.exitFullscreen?.();
+}
+
